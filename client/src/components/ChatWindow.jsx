6f4863d29ea0ef735adb92594f97b75cc787cbc0
mdeplata <126539030+mdeplata@users.noreply.github.com>
2025-08-19 16:20:03 -0400

    Create Base layout for Front-end

---
+++ client/src/components/ChatWindow.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import './styling/ChatWindow.css'
+
+export default function ChatWindow() {
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        setSelectedFile(event.target.files[0])
+    }
+
+    const handleUpload = () => {
+
+    }
+
+    return (
+        <main>
+            <section className="chat-container">
+                <div className="chat-header">
+                    <p>SummarAI: PDF Query Bot</p>
+                </div>
+                <div className="chat-conversation-container">
+                </div>
+                <form className="chat-input-container">
+                    <input name="user-input" type="text" id="user-input" required />
+                    <button id="submit" className="submit-button">
+                        <svg viewBox="0 0 27 33" className="submit-icon">
+                            <path d="M4.53248 16.0788L2.0004 30.7978L24.7891 16.0788L2.00041 1.35974L4.53248 16.0788ZM4.53248 16.0788L14.6608 16.0788" stroke-width="2.53207" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </button>
+                </form>
+                <div className="file-upload-container">
+                    <input type="file" accept=".pdf" onChange={handleFileChange} />
+                    <button onClick = {handleUpload}>Upload PDF</button>
+                </div>
+            </section>
+        </main>
+    )
+}
